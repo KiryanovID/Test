ea5361f362f8dd8b3387df5fca531b997192277a
KiryanovID <87255481+KiryanovID@users.noreply.github.com>
2021-08-03 22:20:39 +0400

    Update
    реализация методов

--- Task/src/ActionClass.java
+++ Task/src/ActionClass.java
@@ -7,18 +7,84 @@ public class ActionClass {
     List<Integer> group1 = new ArrayList<>();
     List<Integer> group2 = new ArrayList<>();
     List<Integer> group3 = new ArrayList<>();
+    int count;
 
-    protected void helpCommand(){
-        System.out.println("add <point>        - добавить в память программы точки, координаты передаются парами чисел через пробел\n" +
-                "                     прим. add 1 1 -2 -3 //добавить 2 точки: (1,1) и (-2,-3) \n"+
-                "print              - напечатать построчно каждую из трех групп (входящие в них точки)\n" +
-                "                     если в группу не попадает ни одна точка, то вывести сообщение, что группа пуста\n" +
-                "                     последней строкой напечатать количество точек, не вошедших ни в одну группу");
+    protected void addCommand(String[] res){
+        for(int i = 1; i < res.length; i +=2) {
+            listX.add(Integer.parseInt(res[i]));
+            listY.add(Integer.parseInt(res[i+1]));
+        }
     }
     protected void printCommand(){
+        int count = 0;
+            if(group1.size() == 0){
+                System.out.println("Группа пуста");
+
+            }
+            else{
+                for(int i= 0; i < group1.size(); i+=2){
+                    System.out.print(group1.get(i) + " "+ group1.get(i+1) + " ");
+                }
+            }
+        if(group2.size() == 0){
+            System.out.println("Группа пуста");
+
+        }
+        else{
+            for(int i= 0; i < group2.size(); i+=2){
+                System.out.print(group2.get(i) + " "+ group2.get(i+1) + " ");
+            }
+        }
+        if(group3.size() == 0){
+            System.out.println("Группа пуста");
+
+        }
+        else{
+            for(int i= 0; i < group3.size(); i+=2){
+                System.out.print(group3.get(i) + " "+ group3.get(i+1) + " ");
+            }
+        }
+        if(count > 0){
+            System.out.println("Точек не вошедших ни в одну группу: " + count);
+        }
+    }
+    protected void printWithParamCommand(int first, int second){
 
     }
-    private void calculate(){
+    protected void removeCommand(int first, int second){
 
+    }
+    protected void calculate(){
+        for(int i = 0; i < listX.size(); i++){
+            int x = listX.get(i);
+            int y = listY.get(i);
+            if(y >= x){
+                group1.add(x);
+                group1.add(y);
+            }
+            else if(y >= Math.sqrt(x)){
+                group2.add(x);
+                group2.add(y);
+            }
+            else if(y >= Math.pow(x,3)){
+                group3.add(x);
+                group3.add(y);
+            }
+            else count+=2;
+        }
+    }
+    protected void clearCommand(){
+        listX.clear();
+        listY.clear();
+    }
+    protected void helpCommand(){
+        System.out.println("add <point>        - добавить в память программы точки, координаты передаются парами чисел через пробел\n" +
+                "print              - напечатать построчно каждую из трех групп (входящие в них точки)\n" +
+                "print <group_num>  - напечатать одним списком точки, входящие в группу(ы) переданную(ые) параметром <group_num>\n"+
+                "remove <group_num> - удалить из памяти все точки, входящие в группу(ы) <group_num>\n"+
+                "clear              - очистить память\n"+
+                "help               - вывод справки по командам\n"+
+                "exit               - выход из программы"
+        );
     }
 }
--- Task/src/Main.java
+++ Task/src/Main.java
@@ -9,24 +9,23 @@ public class Main {
         while (sc.hasNext()){
             s = sc.nextLine();
             if(s.contains("add")){
-                String[] res = s.split(" ");
-                for(int i = 1; i < res.length; i +=2) {
-                    ac.listX.add(Integer.parseInt(res[i]));
-                    ac.listY.add(Integer.parseInt(res[i+1]));
-                }
+                String[] result = s.split(" ");
+                ac.addCommand(result);
+                ac.calculate();
             }
             else if(s.equals("print")){
                 ac.printCommand();
             }
             else if(s.contains("print ")){
-
+                String[] res = s.split(" ");
+                ac.printWithParamCommand(Integer.parseInt(res[1]), Integer.parseInt(res[2]));
             }
             else if(s.equals("remove")){
-
+                String[] result = s.split(" ");
+                ac.removeCommand(Integer.parseInt(result[1]), Integer.parseInt(result[2]));
             }
             else if(s.equals("clear")){
-                ac.listX.clear();
-                ac.listY.clear();
+                ac.clearCommand();
             }
             else if(s.equals("help")){
                 ac.helpCommand();
